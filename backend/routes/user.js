4dab02dc66203bf2171bfe23cee5c2c168c7f0bb
itgbarbara <itgbarbara@gmail.com>
2024-09-06 16:56:43 +0200

    Small correction

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -9,7 +9,7 @@ const router = express.Router();
 // PARAMETRAGE DES ROUTES //
 
 // Route pour ajouter un utilisateur à la base de données (POST)
-router.post('/signup', userCtrl.signup); // éclaration du segment final de la route. Le reste de l'url de la route est déclaré dans app.js
+router.post('/signup', userCtrl.signup); // Déclaration du segment final de la route. Le reste de l'url de la route est déclaré dans app.js
 
 // Route pour connecter un utilisateur (POST)
 router.post('/login', userCtrl.login);
